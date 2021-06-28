d65719fa61db099cc5fc828128f2066a269bafa3
Ody <melodymonyok@gmail.com>
2021-06-28 15:35:42 -0500

    Updated apiRoutes to allow for deleting routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,21 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 const noteLog = JSON.parse(fs.readFileSync('db/db.json'));
-// let noteArray = [];
 
-/* const noteLog = fs.readFile('../db/db.json', 'utf8', (err, notes) => {
+/* const noteLog = JSON.parse(fs.readFile('db/db.json', 'utf8', (err, notes) => {
     if (err) {
         console.log("File read failed:", err);
         return;
     }
-    try {
-        const notes = JSON.parse(fs.readFileSync('../db/db.json'));
-    } catch (err) {
-        console.log(err);
-        return;
-    }
-    console.log('File data:', notes);
-}); */
+})); */
+
+function remove(array, key, value) {
+    const index = array.findIndex(obj => obj[key] === value);
+    return index >= 0 ? [
+        ...array.slice(0, index),
+        ...array.slice(index + 1)
+    ] : array;
+};
 
 module.exports = (app) => {
     // Create new notes.
@@ -26,12 +26,15 @@ module.exports = (app) => {
     app.post('/api/notes', (req, res) => {
         const newNote = req.body;
         newNote.id = uuidv4();
-/*         console.log("What is newNote?");
-        console.log(typeof noteLog);
-        console.log(noteLog);
-        noteArray.push(newNote); */
         noteLog.push(newNote);
         fs.writeFileSync('db/db.json', JSON.stringify(noteLog));
         res.json(noteLog);
+    });
+
+    app.delete('/api/notes', (req, res) => {
+        const delNote = req.body.id;
+        const newLog = remove(noteLog, "id", delNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(newLog));
+        res.json(newLog);
     });
 };
