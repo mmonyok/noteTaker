c188d66759492a97bd275a97d1e11093cac5cf2e
Ody <melodymonyok@gmail.com>
2021-06-28 13:44:57 -0500

    Fixed order of html routes.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = (app) => {
     // Routes
+    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
+
     app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
     app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
-
-    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
 };
