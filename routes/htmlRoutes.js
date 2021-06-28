3c3a1e0960eac81a2bead21275a08c96f4f04a7c
Ody <melodymonyok@gmail.com>
2021-06-27 21:02:06 -0500

    Created htmlRoutes page that holds the get requests for each html page.

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = (app) => {
+    // Routes
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
+}
